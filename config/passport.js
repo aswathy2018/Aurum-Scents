7b0616e6abd2afd723ae0ebdeb90ba01750db626
aswathy2018 <aswathysudhakaran3002@gmail.com>
2025-04-08 09:18:30 +0530

    Google authentication done

--- config/passport.js
+++ config/passport.js
@@ -58,7 +58,7 @@ const env = require('dotenv').config()
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3002/google/callback'
+    callbackURL: 'http://aurum-scents.store/google/callback'
 },
 async(accessToken, refreshToken, profile, done) => {
     try {
